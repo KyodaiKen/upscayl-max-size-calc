0b4d9d5b5760c78cec610bd62eec3cc6e2700ba4
Kyoudai Ken <bloodysword@gmx.net>
2025-04-21 16:53:10 +0200

    Added all files

---
+++ autoscaledir.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Execute the Python script and pass all command-line arguments to it.
+python3 /home/kyo/.scripts/upscayl/autoscaledir.py --upscayl-path "python3 /home/kyo/.scripts/upscayl/autoscale.py" "$@"
